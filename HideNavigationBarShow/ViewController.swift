457e449f935a2e7590290334d56163513d58d965
Looping <www.looping@gmail.com>
2016-04-02 23:50:59 +0800

    新建项目

---
+++ HideNavigationBarShow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HideNavigationBarShow
+//
+//  Created by Looping on 4/2/16.
+//  Copyright © 2016 RidgeCorn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
